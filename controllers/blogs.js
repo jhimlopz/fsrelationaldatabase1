98aa87db9275b3415f7a6102d07415b3eb6da8d1
jhimlopz <cabreralopezjhim@gmail.com>
2026-09-10 17:39:17 +0800

    excercise 8-12: users, error handling, ownership rules

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,15 +1,26 @@
 const router = require("express").Router();
 
-const { Blog } = require("../models");
+const { Blog, User } = require("../models");
+const { tokenExtractor } = require("../util/middleware");
 
 router.get("/", async (req, res) => {
-  const blogs = await Blog.findAll();
+  const blogs = await Blog.findAll({
+    attributes: { exclude: ["userId"] },
+    include: {
+      model: User,
+      attributes: ["name"],
+    },
+  });
   res.json(blogs);
 });
 
-router.post("/", async (req, res, next) => {
+router.post("/", tokenExtractor, async (req, res, next) => {
   try {
-    const blog = await Blog.create(req.body);
+    const user = await User.findByPk(req.decodedToken.id);
+    if (!user) {
+      return res.status(400).json({ error: "invalid user" });
+    }
+    const blog = await Blog.create({ ...req.body, userId: user.id });
     res.json(blog);
   } catch (error) {
     next(error);
@@ -28,7 +39,12 @@ router.get("/:id", blogFinder, async (req, res) => {
   res.json(req.blog);
 });
 
-router.delete("/:id", blogFinder, async (req, res) => {
+router.delete("/:id", tokenExtractor, blogFinder, async (req, res) => {
+  if (req.blog.userId !== req.decodedToken.id) {
+    return res
+      .status(403)
+      .json({ error: "only the creator can delete this blog" });
+  }
   await req.blog.destroy();
   res.status(204).end();
 });
--- util/middleware.js
+++ util/middleware.js
@@ -1,11 +1,35 @@
+const jwt = require("jsonwebtoken");
+const { SECRET } = require("./config");
+
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    try {
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET);
+    } catch {
+      return res.status(401).json({ error: "token invalid" });
+    }
+  } else {
+    return res.status(401).json({ error: "token missing" });
+  }
+  next();
+};
+
 const errorHandler = (error, req, res, next) => {
   console.error(error.message);
 
-  if (error.name === "SequelizeValidationError") {
-    return res.status(400).json({ error: error.message });
+  if (
+    error.name === "SequelizeValidationError" ||
+    error.name === "SequelizeUniqueConstraintError"
+  ) {
+    return res.status(400).json({ error: error.errors.map((e) => e.message) });
+  }
+
+  if (error.name === "JsonWebTokenError") {
+    return res.status(401).json({ error: "invalid token" });
   }
 
   next(error);
 };
 
-module.exports = { errorHandler };
+module.exports = { errorHandler, tokenExtractor };
